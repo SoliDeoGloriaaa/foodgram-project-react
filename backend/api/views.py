e7d23408c6ae24c99340eb9d3b575c6e403580b2
Александр Афанасьев <knowthepain00@mail.ru>
2023-06-18 01:09:19 +0300

    18.06 flake8  Второе ревью =)

--- backend/api/views.py
+++ backend/api/views.py
@@ -6,7 +6,6 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from djoser.views import UserViewSet
 from rest_framework import permissions, status, viewsets
-from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
