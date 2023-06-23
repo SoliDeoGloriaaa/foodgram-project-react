90534193a8d8951b0100720810b8eaeb09afffe9
Александр Афанасьев <knowthepain00@mail.ru>
2023-06-23 12:42:08 +0300

    первое ревью 23.06 =)

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
 
 from .models import (
-    AmountImgredientsInRecipe, FavoriteRecipe, Ingredient, Recipe, Tag
-)
-
-from .models import (
     AmountImgredientsInRecipe,
     FavoriteRecipe,
     Ingredient,
